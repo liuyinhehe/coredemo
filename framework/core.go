2d1c5e6cceb76cf3aa896b52687f7e68d1b27c49
liuyinhe <liuyinhe9980@qq.com>
2021-12-05 04:16:16 -0800

    iiii

--- framework/core.go
+++ framework/core.go
@@ -1,14 +1,17 @@
-package framework
+package framework /*框架代码存放于framwork文件夹中，业务代码存放于文件夹之外*/
 
 import "net/http"
 
+//框架核心结构
 type Core struct {
 }
 
+//初始化框架核心结构
 func NewCore() *Core {
 	return &Core{}
 }
 
+//框架核心结构实现handler接口
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	// TODO
 }
--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 //主函数
 func main() {
 	server := &http.Server{
-		Handler: framework.NewCore(),
-		Addr:    "localhost:8080",
+		Handler: framework.NewCore(), /*handler字段为空会使用默认DefaultServerMux
+		  路由器来填充这个值，一般都使用自定义的请求核心处理函数*/
+		Addr: "localhost:8080",
 	}
+	//通过监听URL地址和控制器函数来创建http服务
 	server.ListenAndServe()
 }
